441fe2f5de5279b121b90b63a83017d79db3f980
GalwayK <97978733+GalwayK@users.noreply.github.com>
2022-02-01 12:55:51 -0500

    Update CardDealer.java
    Added a line printing out the newCard object's value and suit variable values.

--- src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
@@ -15,6 +15,8 @@ public class CardDealer
     {
         Card newCard = new Card("Hearts", 7);
         
+        System.out.printf("Suit: %s, Value: %d\n", newCard.getSuit(), newCard.getValue());
+        
         Card nineClubs = new Card("Clbs", 9);
         nineClubs.setSuit("diamonds");
     }
